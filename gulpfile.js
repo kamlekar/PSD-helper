337692c9acefb3ac80892960f23e70a78a5d3f61
kamlekar <kamlekar.venkatesh@gmail.com>
2018-03-05 18:17:17 +0530

    Clear folder before running the task

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,37 @@
 var gulp    = require('gulp');
 var PSD     = require('psd');
 var rename  = require('gulp-rename');
+var del     = require('del');
 
 
 // Variables
 var PSD_ROOT_PATH       = `psds/`;
 var PSD_SRC_PATH        = `${PSD_ROOT_PATH}*.psd`;
-var PNG_VERSION_PATH    = `output/PNG_Versions/`;
+var OUTPUT              = `output`;
+var PNG_VERSION_PATH    = `${OUTPUT}/PNG_Versions/`;
 
 
 function throughPSDs(){
+    // delete existing files, before starting the task
+    del([
+        `${OUTPUT}/*`
+    ]);
+
     return gulp.src(PSD_SRC_PATH)
-        // Making sure the folder exists
+        // Making sure the folders exists
         .pipe(gulp.dest(PNG_VERSION_PATH))
         .pipe(rename(function(x){
-            var url = PSD_ROOT_PATH + '/' + x.basename + x.extname;
+            var url = `${PSD_ROOT_PATH}/${x.basename}${x.extname}`;
             console.log(url);
             // var psd = PSD.fromFile(url);
             PSD.open(url).then(psd => {
                 console.log(psd);
-                
-                return psd.image.saveAsPng(`${PNG_VERSION_PATH}${x.basename}.png`);
+                var rawFile = `${PNG_VERSION_PATH}${x.basename}`;
+                psd.image.saveAsPng(`${rawFile}.png`);
+                // Delete PSD file
+                del([
+                    `${rawFile}.psd`
+                ]);
             })
             .then(() => console.log("Finished!"));
         }))
